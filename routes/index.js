49a7037c415b5a87e35552e2f249383f25a4bbc7
chengdev <dong862647669@gmail.com>
2023-07-12 10:26:28 +0800

    feat: 文件下载及websocket

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express')
 const expressWs = require('express-ws')
+const { WebSocketServer } = require('ws')
+const fs = require('fs')
+const { resolve } = require('path')
 var router = express.Router()
 expressWs(router)
 
@@ -25,6 +28,17 @@ router.get('/route', (req, res) => {
   })
 })
 
+router.get('/file', (req, res) => {
+  const filePath = resolve(__dirname, '../public/index.html')
+  console.log('filePath: ', filePath)
+  res.status(200).setHeader('Content-Type', 'text/html').sendFile(filePath, {
+    // headers: {
+    // 'Content-Type': 'text/html',
+    // "Content-Disposition": 'attachment;filename=FileName.html'
+    // }
+  })
+})
+
 router.ws('/socketTest', function (ws, req) {
   ws.send('恭喜你连接成功了')
   ws.onopen = (msg) => {
@@ -33,6 +47,25 @@ router.ws('/socketTest', function (ws, req) {
   ws.onmessage = (msg) => {
     ws.send('Hi', msg)
   }
+})
+
+const wss = new WebSocketServer({
+  port: 3001
+})
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error)
+  ws.onopen = () => {
+    console.log('websocket 服务 open')
+  }
+  ws.on('message', (data) => {
+    console.log('received %s', data, data === 'hello')
+    if (data == 'hello') {
+      console.log('收到了消息:', data)
+      // ws.send('收到了消息hello')
+    }
+  })
+
+  ws.send('something')
 })
 
 module.exports = router
