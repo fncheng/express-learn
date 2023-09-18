074e7fbe570d5423f0a67c4d3860fd54fa54b0f2
chengdev <dong862647669@gmail.com>
2023-09-18 18:38:57 +0800

    fix: update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource')
+})
 
-module.exports = router;
+router.get('/getdata', (req, res) => {
+  setTimeout(() => {
+    res.send({
+      errCode: 1004001,
+      msg: '',
+      body: [{ name: 'zs', age: 20 }]
+    })
+  }, 100)
+})
+
+router.get('/getname', (req, res) => {
+  setTimeout(() => {
+    res.send({ code: 200, body: { name: 'zs' } })
+  }, 4000)
+})
+
+module.exports = router
